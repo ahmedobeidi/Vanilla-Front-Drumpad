0d7e2e09ce7cfed332e7dbd29834a216e04623e5
ahmedobeidi <alobeidi.dev@gmail.com>
2024-11-25 14:26:12 +0100

    DONE: Keyup

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-document.addEventListener("keydown", handleKey);
+document.addEventListener("keydown", handleKeyDown);
+document.addEventListener("keyup", handleKeyUp);
 
-function handleKey(event) {
+
+function handleKeyDown(event) {
     const keyCode = event.keyCode;
-    const audio = document.querySelector(`audio[data-key = '${keyCode}']`);
-    const key = document.querySelector(`.key[data-key = '${keyCode}']`);
+    const key = document.querySelector(`.key[data-key='${keyCode}']`);
+    if (!key) return;
     key.classList.add("playing");
+}
+
+function handleKeyUp(event) {
+    const keyCode = event.keyCode;
+    const key = document.querySelector(`.key[data-key='${keyCode}']`);
+    if (!key) return;
+    key.classList.remove("playing");
+    const audio = document.querySelector(`audio[data-key='${keyCode}']`);
     audio.play();
-    setTimeout(() => {key.classList.remove("playing")}, 100);
 }
