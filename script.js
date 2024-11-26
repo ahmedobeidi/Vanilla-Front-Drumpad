3c61bc71a59fbc8f4b6ea92223760b87a4bcb134
ahmedobeidi <alobeidi.dev@gmail.com>
2024-11-26 13:36:05 +0100

    DONE: Record

--- script.js
+++ script.js
@@ -1,25 +1,60 @@
-document.addEventListener("keydown", handleKeyDown);
-document.addEventListener("keyup", handleKeyUp);
-
+window.addEventListener("keydown", handleKeyDown);
+window.addEventListener("keyup", handleKeyUp);
+let nots = [];
+let isRecord = false;
 
 function handleKeyDown(event) {
     const keyCode = event.keyCode;
     const key = document.querySelector(`.key[data-key='${keyCode}']`);
     const audio = document.querySelector(`audio[data-key='${keyCode}']`);
-    // const keyClicked = document.querySelector(`.key[data-clicked="true"]`);
+    if (!key) return;
     if (key.dataset.clicked === "true") return;
     key.dataset.clicked = "true";
-    if (!key) return;
-    console.log(key);
-    key.classList.add("playing");
-    audio.play();
+    if (keyCode != 82 && keyCode != 80) {
+        key.classList.add("playing");
+        audio.currentTime = 0;
+        audio.play();
+    }
 }
 
 function handleKeyUp(event) {
     const keyCode = event.keyCode;
     const key = document.querySelector(`.key[data-key='${keyCode}']`);
+    const audio = document.querySelector(`audio[data-key='${keyCode}']`);
+    if (!isRecord) {
+        notRecordMode(keyCode, key, audio);
+    }
+    else {
+        recordMode(keyCode, key, audio);
+    }
+}
+
+function notRecordMode(keyCode, key, audio) {
     if (!key) return;
-    key.classList.remove("playing");
-    key.dataset.clicked = "false";
-    
+    if (keyCode != 82 && keyCode != 80) {
+        audio.play();
+        key.dataset.clicked = "false";
+        key.classList.remove("playing");
+    } 
+    else if (keyCode === 82) {
+        key.classList.add("playing");
+        isRecord = true;
+        nots = [];
+    }
+}
+
+function recordMode(keyCode, key, audio) {
+    if (!key) return;
+    if (keyCode != 82 && keyCode != 80) {
+        audio.play();
+        key.dataset.clicked = "false";
+        key.classList.remove("playing");
+        nots.push(audio);
+        console.log(nots);
+    } 
+    else if (keyCode === 82) {
+        key.classList.remove("playing");
+        isRecord = false;
+        console.log(nots);
+    }
 }
