9e73760d761c955c7ab06052639b2925e3fc7d36
ahmedobeidi <alobeidi.dev@gmail.com>
2024-11-27 10:22:13 +0100

    DONE: Play

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 window.addEventListener("keydown", handleKeyDown);
 window.addEventListener("keyup", handleKeyUp);
 let notes = [];
-let isRecord = false;
+let isRecording = false;
 let startRecordDate;
 
 function handleKeyDown(event) {    
+
     const keyCode = event.keyCode;
     const key = document.querySelector(`.key[data-key='${keyCode}']`);
     const audio = document.querySelector(`audio[data-key='${keyCode}']`);
+
     if (!key) return;
+
     if (key.dataset.clicked === "true") return;
+
     key.dataset.clicked = "true";
+
     if (keyCode != 82 && keyCode != 80) {
-    key.classList.add("playing");
-    audio.currentTime = 0;
-    audio.play();
+
+        key.classList.add("playing");
+        audio.currentTime = 0;
+        audio.play();
     }
 }
 
 function handleKeyUp(event) {
+
     const keyCode = event.keyCode;
     const key = document.querySelector(`.key[data-key='${keyCode}']`);
     const audio = document.querySelector(`audio[data-key='${keyCode}']`);
+
     if (!key) return;
-    if (!isRecord) {
+
+    if (!isRecording) {
         notRecordMode(keyCode, key, audio);
     } else {
         recordMode(keyCode, key, audio);
@@ -31,14 +40,17 @@ function handleKeyUp(event) {
 }
 
 function notRecordMode(keyCode, key, audio) {
+
     if (!key) return;
+
     if (keyCode != 82 && keyCode != 80) {
         audio.play();
         key.dataset.clicked = "false";
         key.classList.remove("playing");
-    } else if (keyCode === 82) {
+    } 
+    else if (keyCode === 82) {
         key.classList.add("playing");
-        isRecord = true;
+        isRecording = true;
         notes = [];
         startRecordDate = Date.now();
     }
@@ -50,30 +62,30 @@ function notRecordMode(keyCode, key, audio) {
 }
 
 function recordMode(keyCode, key, audio) {
+
     if (!key) return;
+
     if (keyCode != 82 && keyCode != 80) {
         audio.play();
         key.dataset.clicked = "false";
         key.classList.remove("playing");
-        notes.push({
-        key: keyCode,
-        timeCode: Date.now() - startRecordDate,
-        });
+        notes.push({ key: keyCode,timeCode: Date.now() - startRecordDate });
         // console.log(notes);
-    } else if (keyCode === 82) {
+    } 
+    else if (keyCode === 82) {
         key.classList.remove("playing");
-        isRecord = false;
+        isRecording = false;
     }
 }
 
 function playSound(key) {
+
     notes.forEach((note) => {
         const audio = document.querySelector(`audio[data-key='${note.key}']`);
         setTimeout(() => {
             audio.play();
         }, note.timeCode);
     });
+
     key.classList.remove("playing");
-//   const eventKeyDown = new KeyboardEvent("keydown", {keyCode: unKeyCode })
-//   document.dispatchEvent(eventKeyDown);
 }
